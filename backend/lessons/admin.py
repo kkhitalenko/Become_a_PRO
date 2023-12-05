6ff5f6b41c85732d2979e55fdd292493cbe40fa9
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-12-05 19:25:35 +0300

    Added Answer and Question models

--- backend/lessons/admin.py
+++ backend/lessons/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from lessons.models import Answer, Question
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'correct_option']
--- backend/lessons/models.py
+++ backend/lessons/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    text = models.CharField(max_length=50, verbose_name='Ответ')
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+    def __str__(self):
+        return self.text
+
+
+class Question(models.Model):
+    text = models.TextField(verbose_name='Вопрос')
+    options = models.ManyToManyField(Answer, related_name='options')
+    correct_option = models.ForeignKey(Answer, on_delete=models.PROTECT,
+                                       verbose_name='Верный ответ',
+                                       related_name='correct_option')
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return self.text
