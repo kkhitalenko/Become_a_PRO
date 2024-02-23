578d13269526925002bdadbd3a83dc0f308cec64
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-02-23 21:19:05 +0300

    created viewsets for languages, topics, lessons, questions

---
+++ backend/api/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+
+from api.serializers import LanguageSerializer, TopicSerializer, LessonSerializer, QuestionSerializer
+from lessons.models import Language, Lesson, Topic, Question
+
+
+class LanguageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+
+
+class TopicViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+
+
+class LessonViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
