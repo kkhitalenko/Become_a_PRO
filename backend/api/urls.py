d1ea2ef8c0a0b0cc3b9c3da6845506a03d54e6a2
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-02-23 21:14:09 +0300

    added router for viewing languages, topics, lessons, questions

---
+++ backend/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from . import views
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+router.register(r'languages', views.LanguageViewSet)
+router.register(r'topics', views.TopicViewSet)
+router.register(r'lessons', views.LessonViewSet)
+router.register(r'questions', views.QuestionViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
