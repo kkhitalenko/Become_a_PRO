2cb79e0467a0740082124af4687bbe9b069280a0
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-02-26 21:56:11 +0300

    created serializers for languages, topics, lessons, questions

---
+++ backend/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from lessons.models import Language, Lesson, Question, Topic
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ['name', 'description']
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    language = serializers.CharField()
+
+    class Meta:
+        model = Topic
+        fields = ['language', 'name']
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    topic = serializers.CharField()
+
+    class Meta:
+        model = Lesson
+        fields = ['topic', 'name', 'theory']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    lesson = serializers.CharField()
+    # options = 
+    correct_option = serializers.CharField()
+
+    class Meta:
+        model = Question
+        fields = ['lesson', 'text', 'options', 'correct_option']
