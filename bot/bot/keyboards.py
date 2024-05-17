3e34456dc7f016e689b35f2324744fb181eefdbd
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-05-17 14:48:31 +0300

    PEP8

--- bot/bot/keyboards.py
+++ bot/bot/keyboards.py
@@ -2,6 +2,12 @@ from aiogram import types
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 languages_builder = InlineKeyboardBuilder()
-languages_builder.row(types.InlineKeyboardButton(text='Python', url='http://127.0.0.1:8000/api/v1/topics/'))
-languages_builder.row(types.InlineKeyboardButton(text='Go', url='http://127.0.0.1:8000/api/v1/topics/'))
-languages_builder.row(types.InlineKeyboardButton(text='Rust', url='http://127.0.0.1:8000/api/v1/topics/'))
+languages_builder.row(types.InlineKeyboardButton(
+    text='Python', url='http://127.0.0.1:8000/api/v1/topics/'
+))
+languages_builder.row(types.InlineKeyboardButton(
+    text='Go', url='http://127.0.0.1:8000/api/v1/topics/'
+))
+languages_builder.row(types.InlineKeyboardButton(
+    text='Rust', url='http://127.0.0.1:8000/api/v1/topics/'
+))
