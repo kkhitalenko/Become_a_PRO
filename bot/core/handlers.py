4dc17aba50d7364d615a68fcb35593a087a2e673
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-05-22 20:14:04 +0300

    refactoring

---
+++ bot/core/handlers.py
@@ -0,0 +1,12 @@
+from aiogram import Router, types
+from aiogram.filters.command import Command
+
+from core import keyboards, messages
+
+router = Router()
+
+
+@router.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer(messages.START_MESSAGE,
+                         reply_markup=keyboards.languages_builder.as_markup())
--- bot/main.py
+++ bot/main.py
@@ -2,20 +2,14 @@ import asyncio
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import CommandStart
+from aiogram import Bot, Dispatcher
 
-from core import keyboards, messages
+from core.handlers import router
 
 TOKEN = os.getenv('TOKEN')
 
 dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def cmd_start(message: types.Message):
-    await message.answer(messages.START_MESSAGE,
-                         reply_markup=keyboards.languages_builder.as_markup())
+dp.include_router(router)
 
 
 async def main():
