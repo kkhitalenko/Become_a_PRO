43558db8eb1fd5c810ba1fa652b8d773f713778e
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-06-11 20:54:58 +0300

    add python-dotenv

--- backend/Become_a_PRO/settings.py
+++ backend/Become_a_PRO/settings.py
@@ -1,4 +1,10 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+jqux7=8v)f)o&t&@-b)xb(_z2(9&0%ar(ur%tw1vj5i51jm9e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
