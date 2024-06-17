c2628ce849eb6939718a987172cbda7bf9443ab6
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-06-17 22:40:26 +0300

    delete tg_user app

--- backend/tg_user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from tg_user.models import TelegramUser
-
-
-@admin.register(TelegramUser)
-class TelegramUserAdmin(admin.ModelAdmin):
-    list_display = ['tg_user_id', 'username']
-    list_per_page = 20
--- backend/tg_user/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TgUserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tg_user'
-    verbose_name = 'Пользователи бота'
