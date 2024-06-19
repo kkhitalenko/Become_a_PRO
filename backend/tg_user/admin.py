862e3f271e005dff34239079e8b12b2060aed53c
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-06-19 22:50:48 +0300

    delete tg_user app

--- backend/tg_user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from tg_user.models import TelegramUser
-
-
-@admin.register(TelegramUser)
-class TelegramUserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'tg_user_id']
-    list_per_page = 20
