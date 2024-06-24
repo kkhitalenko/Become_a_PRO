eff30abf9d722f3cc1714d680d81191918268562
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-06-24 14:09:35 +0300

    refactoring

--- bot/core/endpoints.py
+++ bot/core/endpoints.py
@@ -1,3 +1,2 @@
 LANGUAGES = 'http://127.0.0.1:8000/api/v1/languages/'
-# USERS = 'http://127.0.0.1:8000/api/v1/tgusers/'
 PROGRESS = 'http://127.0.0.1:8000/api/v1/progress/'
--- bot/main.py
+++ bot/main.py
@@ -6,21 +6,21 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 from core.commands import set_commands
-from core.handlers import router
 
 
 load_dotenv()
-
-
 TOKEN = os.getenv('TOKEN')
 
 dp = Dispatcher()
-dp.include_router(router)
+bot = Bot(token=TOKEN)
 
 
 async def main():
-    bot = Bot(token=TOKEN)
+    from core.handlers import router
+    dp.include_router(router)
+
     await set_commands(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
