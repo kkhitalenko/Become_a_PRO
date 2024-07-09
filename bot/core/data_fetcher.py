8626317f6a39045ba00bf5e9ca70802fa044832c
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-07-09 15:05:29 +0300

    add getting lesson function from Django

--- bot/core/data_fetcher.py
+++ bot/core/data_fetcher.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import aiohttp
 
 from core import endpoints
@@ -23,16 +25,14 @@ async def create_progress(tg_user_id: int, language: str,
             return resp.status
 
 
-async def check_progress(tg_user_id: int, language: str) -> bool:
-    """Returns True if progress exists. Otherwise returns False."""
+async def get_progress(tg_user_id: int, language: str) -> Optional[int]:
+    """Returns lesson number if progress exists. Otherwise returns None."""
 
     async with aiohttp.ClientSession() as session:
         progress_url = f'{endpoints.PROGRESS}{language}_{tg_user_id}/'
         async with session.get(progress_url) as resp:
-            if resp.status == 200:
-                return True
-            elif resp.status == 404:
-                return False
+            result = await resp.json()
+            return result.get('last_completed_lesson')
 
 
 async def update_progress(tg_user_id: int, language: str,
@@ -45,3 +45,13 @@ async def update_progress(tg_user_id: int, language: str,
         async with session.patch(f'{endpoints.PROGRESS}{progress}/',
                                  data=payload) as resp:
             return resp.status
+
+
+async def get_lesson(language: str, last_completed_lesson: int):
+    """Returns json including theory and list of questions."""
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f'{endpoints.LESSONS}{language}/{last_completed_lesson+1}/',
+        ) as resp:
+            return await resp.json()
--- bot/core/endpoints.py
+++ bot/core/endpoints.py
@@ -1,2 +1,3 @@
 LANGUAGES = 'http://127.0.0.1:8000/api/v1/languages/'
 PROGRESS = 'http://127.0.0.1:8000/api/v1/progress/'
+LESSONS = 'http://127.0.0.1:8000/api/v1/lessons/'
