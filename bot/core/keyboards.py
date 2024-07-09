b3e770854ee23007087db85b72db8c0ab087667c
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-07-09 19:01:22 +0300

    add a dynamic keyboard function + refactoring

--- bot/core/keyboards.py
+++ bot/core/keyboards.py
@@ -1,29 +1,39 @@
+from aiogram.types import List
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_langeages_kb():
     """Returns the keyboard with 'Python', 'Go' and 'Rust' buttons."""
 
-    languages = InlineKeyboardBuilder()
-    languages.button(text='Python', callback_data='python')
-    languages.button(text='Go', callback_data='go')
-    languages.button(text='Rust', callback_data='rust')
-    return languages.as_markup()
+    kb = InlineKeyboardBuilder()
+    kb.button(text='Python', callback_data='python')
+    kb.button(text='Go', callback_data='go')
+    kb.button(text='Rust', callback_data='rust')
+    return kb.as_markup()
 
 
 def get_yes_no_kb():
     """Returns the keyboard with 'yes' and 'no' buttons."""
 
-    yes_no = InlineKeyboardBuilder()
-    yes_no.button(text='да', callback_data='да')
-    yes_no.button(text='нет', callback_data='нет')
-    return yes_no.as_markup()
+    kb = InlineKeyboardBuilder()
+    kb.button(text='да', callback_data='yes')
+    kb.button(text='нет', callback_data='no')
+    return kb.as_markup()
 
 
 def get_continue_or_reset_kb():
     """Returns the keyboard with 'continue' and 'reset' buttons."""
 
-    yes_no = InlineKeyboardBuilder()
-    yes_no.button(text='продолжить', callback_data='continue')
-    yes_no.button(text='начать заново', callback_data='reset')
-    return yes_no.as_markup()
+    kb = InlineKeyboardBuilder()
+    kb.button(text='продолжить', callback_data='continue')
+    kb.button(text='начать заново', callback_data='reset')
+    return kb.as_markup()
+
+
+def create_kb(buttons: List[str]):
+    """Receive a list of buttons and returns a keyboard with those buttons."""
+
+    kb = InlineKeyboardBuilder()
+    for btn in buttons:
+        kb.button(text=btn.title(), callback_data=btn)
+    return kb.as_markup()
