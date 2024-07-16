46ea934dd3aab358a4e5fc35df951670a8e80403
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-07-16 20:41:42 +0300

    rename command: switch_mode -> repeat

--- bot/core/commands.py
+++ bot/core/commands.py
@@ -5,7 +5,7 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_commands(bot: Bot):
     commands = [BotCommand(command='start', description='начать обучение'),
                 BotCommand(command='continue ', description='продолжить'),
-                BotCommand(command='switch_mode',
-                           description='переключить режим'),
+                BotCommand(command='repeat',
+                           description='повторить сложные вопросы'),
                 BotCommand(command='help', description='помощь'),]
     await bot.set_my_commands(commands, BotCommandScopeDefault())
