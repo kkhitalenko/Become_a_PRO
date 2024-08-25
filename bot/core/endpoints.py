900f00c7b61b9e2310ddc128062890bf9daaa824
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-08-25 17:04:47 +0300

    configure docker container for telegram-bot

--- bot/core/endpoints.py
+++ bot/core/endpoints.py
@@ -1,3 +1,9 @@
-LANGUAGES = 'http://127.0.0.1:8000/api/v1/languages/'
-PROGRESS = 'http://127.0.0.1:8000/api/v1/progress/'
-LESSONS = 'http://127.0.0.1:8000/api/v1/lessons/'
+# для запуска через docker-compose
+LANGUAGES = 'http://nginx/api/v1/languages/'
+PROGRESS = 'http://nginx/api/v1/progress/'
+LESSONS = 'http://nginx/api/v1/lessons/'
+
+# для запуска на локальной машине
+# LANGUAGES = 'http://127.0.0.1/api/v1/languages/'
+# PROGRESS = 'http://127.0.0.1/api/v1/progress/'
+# LESSONS = 'http://127.0.0.1/api/v1/lessons/'
